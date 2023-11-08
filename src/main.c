1d3deaf6c2ca60ebc56847a34f9e5e5c09f38d71
김재원 <jajwon202@naver.com>
2023-11-08 13:19:07 +0000

    polynomial

--- src/main.c
+++ src/main.c
@@ -1,95 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_LIST_SIZE 100
-
-typedef int element;
+#define MAX(a,b) (((a)>(b)?(a):(b)))
+#define MAX_DEGREE 101
 typedef struct{
-	element array[MAX_LIST_SIZE];
-	int size;
-}ArrayListType;
+	int degree;
+	float coef[MAX_DEGREE];
+} polynomial;
 
-void error(char *message){
-	fprintf(stderr,"%s\n",message);
-	exit(1);
-}
+int start= 0; // 두 다항식의 최고차항의 계수 합이 0이 아닌 차수까지 이동시켜주는 포인터
 
-void init_List(ArrayListType *L){
-	L->size =0;
-}
-
-
-int is_empty(ArrayListType *L){
-	return L->size ==0;
-}
-
-
-int is_full(ArrayListType *L){
-	return L->size == MAX_LIST_SIZE;
-}
-
-element get_entry(ArrayListType *L,int pos){
-	if(pos<0 || pos>L->size){
-		error("위치 오류");
+polynomial poly_add1(polynomial A,polynomial B){
+	polynomial C;
+	int Apos=0; 
+	int Bpos=0; 
+	int Cpos=0;
+	int degree_a = A.degree;
+	int degree_b = B.degree;
+	int Apointer = A.degree; // 최고차항을 가르키는 포인터
+	
+	C.degree = MAX(A.degree,B.degree);
+	
+	while(Apos<=A.degree && Bpos<=B.degree){
+		if(degree_a>degree_b){
+			C.coef[Cpos++]=A.coef[Apos++];
+			degree_a--;
+		}
+		else if(degree_a == degree_b){ // 두 다항식 둘다
+			if(degree_a==Apointer){ // 최고차항이라면
+				if(A.coef[start]+B.coef[start]==0){ //최고차항의 계수의 합이 0인지 확인
+					start++; // 포인터를 옮겨준다
+					Apointer--;
+				}
+			}
+		
+			C.coef[Cpos++]= A.coef[Apos++] + B.coef[Bpos++];
+			degree_a--;
+			degree_b--;
+		}
+		else{
+			C.coef[Cpos++] = B.coef[Bpos++];
+			degree_b--;
+		}
 	}
-	return L->array[pos];
+	return C;
 }
 
-void print_List(ArrayListType *L){
-	for(int i=0; i<L->size; i++){
-		printf("%d -> ",L->array[i]);
+void print_poly(polynomial p){
+	for(int i=start;i<p.degree;i++){ // 포인터 위치부터 출력을 시작해준다
+		printf("%3.lfx^%d + ",p.coef[i],p.degree-i);
 	}
-	printf("\n");
+	
+	printf("%3.lf\n",p.coef[p.degree]);
 }
-
-void insert_last(ArrayListType *L, element item){
-	if(is_full(L)){
-		error("리스트 오버플로우");
-	}
-	L->array[L->size++] = item;
-}
-
-void insert(ArrayListType *L, int pos, element item){
-	if(is_full(L)){
-		error("리스트 오버플로우");
-	}
-	if(pos<0 || pos>L->size){
-		error("위치 오류1");
-	}
-	for(int i=L->size-1;i>=pos;i--){
-		L->array[i+1] = L->array[i];
-	}
-	L->array[pos] = item;
-	L->size ++;
-}
-
-element delete(ArrayListType *L, int pos){
-	if(pos<0 || pos>L->size){
-		error("위치 오류");
-	}
-	element item = L->array[pos];
-	for(int i=pos; i<L->size-1;i++){
-		L->array[i] = L->array[i+1];
-	}
-	L->size--;
-	return item;
-}
-
-
 
 int main(){
-	ArrayListType list;
-	init_List(&list);
+	polynomial a= {4,{6,0,0,0,10}};
+	polynomial b= {4,{-6,0,5,0,1}};
+	polynomial c;
 	
-	insert(&list,0,10);
-	print_List(&list);
-	insert(&list,0,20);
-	print_List(&list);
-	insert(&list,0,30);
-	print_List(&list);
-	insert_last(&list,40);
-	print_List(&list);
-	delete(&list,0);
-	print_List(&list);
+	print_poly(a);
+	print_poly(b);
+	c= poly_add1(a,b);
+	printf("-------------------------------\n");
+	print_poly(c);
 	return 0;
 }
