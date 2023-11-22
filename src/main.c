0b107a96f23baba207b2b6b51194ab8f440735a2
김재원 <jajwon202@naver.com>
2023-11-22 14:15:45 +0000

    7linked_stack.c

---
+++ src/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef int element;
+typedef struct StackNode{
+	element data;
+	struct StackNode* link;
+}StackNode;
+
+typedef struct{
+	StackNode *top;
+}LinkedStackType;
+
+void init(LinkedStackType *s){
+	s->top = NULL;
+}
+
+int is_empty(LinkedStackType *s){
+	return (s->top == NULL);
+}
+
+void push(LinkedStackType *s,element item){
+	StackNode *temp = (StackNode*)malloc(sizeof(StackNode));
+	temp->data = item;
+	temp->link = s->top;
+	s->top = temp;
+}
+
+void print_stack(LinkedStackType *s){
+	for(StackNode *p = s->top; p!=NULL; p= p->link){
+		printf("%d-> ",p->data);
+	}
+	printf("NULL\n");
+}
+
+element pop(LinkedStackType *s){
+	if(is_empty(s)){
+		fprintf(stderr,"스택 빔\n");
+		exit(1);
+	}
+	else{
+		StackNode *temp = s->top;
+		int data = temp ->data;
+		s->top = temp->link;
+		free(temp);
+		return data;
+	}
+}
+
+int main(void){
+	LinkedStackType *s=(LinkedStackType*)malloc(sizeof(LinkedStackType));
+	init(s);
+	push(s,1);
+	print_stack(s);
+	push(s,2);
+	print_stack(s);
+	push(s,3);
+	print_stack(s);
+	pop(s);
+	print_stack(s);
+	pop(s);
+	print_stack(s);
+	pop(s);
+	print_stack(s);
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
