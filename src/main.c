ae47270f1f04342a51be4426433f8fc67345f016
김재원 <jajwon202@naver.com>
2024-01-07 14:55:05 +0000

    13index_search.c

--- src/main.c
+++ src/main.c
@@ -1,65 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #define MAX_SIZE 10
-#define SWAP(x,y,t) ( (t)=(x), (x)=(y), (y)=(t))
+#define INDEX_SIZE 256
 
 int list[MAX_SIZE];
-int n;
 
-int sorted[MAX_SIZE];
+typedef struct{
+	int key;
+	int index;
+}itable;
 
-int binary_search(int key, int low, int high){
-	int middle;
+itable index_list[INDEX_SIZE];
+
+int seq_search(int key, int low, int high){
+	int i;
+	for(i=low;i<=high;i++){
+		if(list[i]==key){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int index_search(int key,int n){
+	int i,low,high;
 	
-	if(low<high){
-		middle = (low+high) /2;
-		if(key == list[middle]){
-			return middle;
-		}
-		else if(key <list[middle]){
-			return binary_search(key, low, middle-1);
-		}
-		else{
-			return binary_search(key,middle+1,high);
-		}
+	if(key<list[0] || key>list[n-1]) return -1;
+	
+	for(i=0; i<INDEX_SIZE; i++){
+		if(index_list[i].key <=key && index_list[i].key>key) break;
 	}
-	return -1;
+	
+	if(i == INDEX_SIZE){
+		low = index_list[i-1].index;
+		high = n;
+	}
+	else{
+		low = index_list[i].index;
+		high = index_list[i+1].index;
+	}
+	return seq_search(key,low,high);
 }
 
-int binary_search_itr(int key, int low, int high){
-	int middle;
-	while(low<high){
-		middle = (low+high)/2;
-		if(key == list[middle]){
-			return middle;
-		}
-		else if(key<list[middle]){
-			high = middle -1;
-		}
-		else{
-			low = middle +1;
-		}
-	}
-	return -1;
-}
 
 int main(){
-	list[0] = 1;
-	list[1] = 2;
-	list[2] = 3;
-	list[3] = 5;
-	list[4] = 6;
-	list[5] = 8;
-	list[6] = 9;
-	list[7] = 10;
+	list[0] = 3;
+	list[1] = 9;
+	list[2] = 15;
+	list[3] = 22;
+	list[4] = 31;
+	list[5] = 55;
+	list[6] = 67;
+	list[7] = 88;
+	list[8] = 91;
+	
+	index_list[0].key = 3;
+	index_list[0].index = 0;
+
+	index_list[1].key = 22;
+	index_list[1].index = 3;
+
+	index_list[2].key = 67;
+	index_list[2].index = 6;
+
 
 	
-	if(binary_search_itr(5,0,7) == -1){
+	if(index_search(31,9) == -1){
 		printf("탐색 실패 \n");
 	}
 	else{
-		printf("인덱스 %d에서 키값 탐색\n",binary_search_itr(5,0,7));
+		printf("인덱스 %d에서 키값 탐색\n",index_search(31,9));
 	}
 	
 	return 0;
